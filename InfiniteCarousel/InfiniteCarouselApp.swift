42ec4a9b1f6873de8668ecf51960223c105449e7
Evgeny Ezub <y.yezub@gmail.com>
2025-02-06 10:07:16 +0800

    Initial Commit

---
+++ InfiniteCarousel/InfiniteCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteCarouselApp.swift
+//  InfiniteCarousel
+//
+//  Created by Mr.John on 06/02/25.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
